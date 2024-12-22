059c81d4c3b07420fd8e15f1587e918c12607043
enesmalikdmn <m.duman@digitastic.app>
2024-12-22 14:08:20 +0300

    feat: added route structure

---
+++ app/job-listings/[id]/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'Job Detail',
+    description: 'Find your dream job!',
+}
+
+const JobDetail = () => {
+    return (
+      <div style={{ display: 'flex' }}>
+        Job detail
+      </div>
+    );
+  };
+  
+  export default JobDetail;
---
+++ app/job-listings/page.tsx
@@ -0,0 +1,17 @@
+// pages/job-listings.tsx
+// import JobList from '../app/components/JobList';
+// import Sidebar from '../app/components/Sidebar';
+export const metadata = {
+    
+  title: 'Job Listings',
+  description: 'Find your dream job!',
+}
+const JobListingsPage = () => {
+  return (
+    <div style={{ display: 'flex' }}>
+      Job listing
+    </div>
+  );
+};
+
+export default JobListingsPage;
